5735a5e6f1417bd81105a26cb35e382321fc15a6
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-04-23 13:21:12 -0300

    Updated Constructor and Getters and Setters for Vertice class

--- Taller1/src/main/java/arreglo_de_objetos/Vertice.java
+++ Taller1/src/main/java/arreglo_de_objetos/Vertice.java
@@ -12,5 +12,28 @@ package arreglo_de_objetos;
  * @author Videla Guliotti Tobías Uriel
  */
 public class Vertice {
+    private String name;
+    private int number;
+
+    public Vertice(String name, int number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
     
 }
