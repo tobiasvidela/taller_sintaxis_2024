ef3f8bb3569f0a84521b23937cfafd57f89d7641
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-04-23 13:17:24 -0300

    Updating Main.java and creating Vertice.java

--- Taller1/src/main/java/arreglo_de_objetos/Main.java
+++ Taller1/src/main/java/arreglo_de_objetos/Main.java
@@ -1,3 +1,15 @@
+/**
+ * Cree un Arreglo de vértices cuya dimensión es de 5.
+ * Cada vértice constará de un nombre y de un número.
+ * 
+ * Implementar:
+ *      check_vertice : verificar la existencia de un vértice dentro del arreglo.
+ *      isEmpty_vertices : verificar si el arreglo de vértices esta vacío.
+ *      insert_vertice : dar de alta un vértice dentro del arreglo.
+ *      update_vertice : modificar el número de un vértice que se encuentre dentro del arreglo.
+ *      delete_vertice : dar de baja un vértice dentro del arreglo.
+ * 
+ */
 package arreglo_de_objetos;
 
 /**
---
+++ Taller1/src/main/java/arreglo_de_objetos/Vertice.java
@@ -0,0 +1,16 @@
+/*
+ *  check_vertice : verificar la existencia de un vértice dentro del arreglo.
+ *  isEmpty_vertices : verificar si el arreglo de vértices esta vacío.
+ *  insert_vertice : dar de alta un vértice dentro del arreglo.
+ *  update_vertice : modificar el número de un vértice que se encuentre dentro del arreglo.
+ *  delete_vertice : dar de baja un vértice dentro del arreglo.
+ */
+package arreglo_de_objetos;
+
+/**
+ *
+ * @author Videla Guliotti Tobías Uriel
+ */
+public class Vertice {
+    
+}
