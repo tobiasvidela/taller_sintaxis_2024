6104e066651a683e2744b25b02375fed6e10d6b3
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-04-25 12:01:53 -0300

    Import Scanner class and creating arrays of 5 Vertice objects

--- Taller1/src/main/java/arreglo_de_objetos/Vertice.java
+++ Taller1/src/main/java/arreglo_de_objetos/Vertice.java
@@ -1,10 +1,3 @@
-/*
- *  check_vertice : verificar la existencia de un vértice dentro del arreglo.
- *  isEmpty_vertices : verificar si el arreglo de vértices esta vacío.
- *  insert_vertice : dar de alta un vértice dentro del arreglo.
- *  update_vertice : modificar el número de un vértice que se encuentre dentro del arreglo.
- *  delete_vertice : dar de baja un vértice dentro del arreglo.
- */
 package arreglo_de_objetos;
 
 /**
