c850fd9d5c5feaa27f195a7895f33a876d33bcc6
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-04-29 17:31:06 -0300

    Created project named 'Taller2'

---
+++ Taller2/src/main/java/tobias/taller2/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tobias.taller2;
+
+/**
+ *
+ * @author NETBOOK G5
+ */
+public class Main {
+    
+}
