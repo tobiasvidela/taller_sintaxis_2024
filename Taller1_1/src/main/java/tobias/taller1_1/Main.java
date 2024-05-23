143ea4d79d8c76a441387c1c5fab9365e82b8dff
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-05-22 21:18:33 -0300

    Re-starting project Taller1_1

---
+++ Taller1_1/src/main/java/tobias/taller1_1/Main.java
@@ -0,0 +1,11 @@
+package tobias.taller1_1;
+
+/**
+ * 
+ * @author Videla Guliotti Tobías Uriel
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
